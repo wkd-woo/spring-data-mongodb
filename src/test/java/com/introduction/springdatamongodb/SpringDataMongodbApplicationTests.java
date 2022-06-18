2c476fb48423d443fc09ca95d288c1bb1f677719
wkd-woo <wkdwoos@gmail.com>
2022-06-18 14:24:46 +0900

    first commit & hello world

---
+++ src/test/java/com/introduction/springdatamongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.introduction.springdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
